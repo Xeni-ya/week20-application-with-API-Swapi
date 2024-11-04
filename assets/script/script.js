d397c89093c24e96d5cd06e24d7c93f0039f5ba4
Xeni-ya <budyuginaxeniya@gmail.com>
2024-11-05 00:42:31 +0300

    feat: script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,32 +1,30 @@
+const select = document.getElementById('select').value;
+const inputNumberId = document.querySelector('input-number').value;
+const loadingDiv = document.querySelector('.loading');
+const resultDiv = document.querySelector('.result');
+const errorDiv = document.querySelector('.error');
+
 document.querySelector('.form').addEventListener('submit', function(evt) {
   evt.preventDefault(); 
-
-  const select = document.getElementById('select').value;
-  const id = document.querySelector('input-number').value;
-  const loadingDiv = document.querySelector('.loading');
-  const resultDiv = document.querySelector('.result');
-  const errorDiv = document.querySelector('.error');
-
   // Сбрасываем предыдущие сообщения
   resultDiv.textContent = '';
   errorDiv.textContent = '';
 
-  loadingDiv.style.display = 'block';
-
-  fetch(`https://swapi.py4e.com/${select}/${id}/`)
+  loadingDiv.style.display = 'block'; 
+  fetch(`https://swapi.py4e.com/${select}/${inputNumberId}/`)
       .then(response => {
-        loadingDiv.style.display = 'none'; 
-          if //...
+        if (!response.ok) {
+          return Promise.reject(response.status);
+        }
+        return response.json();
       })
+        
       .then(data => {
-          resultDiv.textContent = JSON.stringify(); 
+        loadingDiv.style.display = 'none';
+        resultDiv.textContent = JSON.stringify(); 
       })
       .catch(error => {
         loadingDiv.style.display = 'none'; 
-          if (error === 404) {
-              errorDiv.textContent = '';
-          } else {
-              errorDiv.textContent = '';
-          }
+        errorDiv.innerHTML = `Ошибка: ${error === 404 ? 'Сервер не доступен' : 'Что-то пошло не так'}`;
       });
 });
