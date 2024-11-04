ffc8f028522f79456cb53836c5691a9436c6096f
Xeni-ya <147890566+Xeni-ya@users.noreply.github.com>
2024-11-05 01:04:29 +0300

    Update script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,19 @@
 const select = document.getElementById('select').value;
 const inputNumberId = document.querySelector('input-number').value;
-const loadingDiv = document.querySelector('.loading');
-const resultDiv = document.querySelector('.result');
-const errorDiv = document.querySelector('.error');
 
-document.querySelector('.form').addEventListener('submit', function(evt) {
+const form = document.querySelector('.form');
+form.addEventListener('submit', function(evt) {
   evt.preventDefault(); 
+
+  const loadingDiv = document.querySelector('.loading');
+  const resultDiv = document.querySelector('.result');
+  const errorDiv = document.querySelector('.error');
   // Сбрасываем предыдущие сообщения
   resultDiv.textContent = '';
   errorDiv.textContent = '';
-
+  // индикатор загрузки
   loadingDiv.style.display = 'block'; 
+
   fetch(`https://swapi.py4e.com/${select}/${inputNumberId}/`)
       .then(response => {
         if (!response.ok) {
@@ -21,10 +24,15 @@ document.querySelector('.form').addEventListener('submit', function(evt) {
         
       .then(data => {
         loadingDiv.style.display = 'none';
+
         resultDiv.textContent = JSON.stringify(); 
       })
       .catch(error => {
         loadingDiv.style.display = 'none'; 
-        errorDiv.innerHTML = `Ошибка: ${error === 404 ? 'Сервер не доступен' : 'Что-то пошло не так'}`;
+
+        errorDiv.innerHTML = `Ошибка: ${error === 405 ? 'Страница недоступна' : ''}`;
+      })
+      .finally(() => {
+
       });
 });
